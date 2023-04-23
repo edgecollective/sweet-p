4e56872d0b088f22f655851f5da2686ae31e2408
Don Blair <donblair@gmail.com>
2023-04-23 16:37:36 -0400

    update

--- firmware/v3.0/base.py
+++ firmware/v3.0/base.py
@@ -6,6 +6,17 @@ import board
 import busio
 import digitalio
 import adafruit_rfm9x
+import ipaddress
+import ssl
+import wifi
+import socketpool
+import adafruit_requests
+import microcontroller
+import watchdog
+
+wdt = microcontroller.watchdog
+wdt.timeout = 10
+wdt.mode = watchdog.WatchDogMode.RESET
 
 node_id = 3
 
@@ -28,6 +39,9 @@ spi = board.SPI()
 # Initialze RFM radio
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 
+# reset the lora chip
+rfm9x.reset()
+
 # identity of this node
 rfm9x.node = node_id
 # enable CRC checking
@@ -39,6 +53,27 @@ rfm9x.ack_delay = 0.1
 # initialize counter
 counter = 0
 ack_failed_counter = 0
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+try:
+    print("Connecting to %s ..."%secrets["ssid"])
+    wifi.radio.connect(secrets["ssid"], secrets["password"])
+    print("Connected to %s!"%secrets["ssid"])
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+except:
+    print("can't connect to wifi")
+
+base_url = "http://bayou.pvos.org/data/"
+public_key = secrets["bayou_public"]
+private_key = secrets["bayou_private"]
+JSON_POST_URL = base_url+public_key
 
 def send(node_to,payload):
     rfm9x.destination = node_to
@@ -54,24 +89,79 @@ def receive():
         node_from = packet[1]
         payload = "{0}".format(packet[4:])
         rssi = rfm9x.last_rssi
-        #print("<--",node_from,"("+str(rssi)+")",payload)
+        print("<--",node_from,"("+str(rssi)+")",payload)
         #print("<--",node_from,"["+str(rssi)+"]",payload)
         #print
         return(packet)
     else:
         return(None)
 
-#print("Waiting for packets...")
-time_now = time.monotonic()
+print("Waiting for packets...")
 
 while True:
     packet = receive()
+    wdt.feed()
     if packet is not None:
-        payload = "{0}".format(packet[4:])
-        #print(payload)
-        c=eval(str(payload,"ascii"))
-        #print(c)
-        d=str(c,"ascii")
-        print(d)
-        #e=d.split(",")
-        #print(e)
+        try:
+            payload = "{0}".format(packet[4:])
+            #print(payload)
+            c=eval(str(payload,"ascii"))
+            #print(c)
+            d=str(c,"ascii")
+            #decs=d.strip()
+            decs=d.split(",")
+            feed_id = int(decs.pop(0))
+            N=2
+            sensors = [decs[n:n+N] for n in range(0, len(decs), N)]
+            #print(feed_id,sensors)
+
+
+            # if not connected to wifi, connect
+            if(wifi.radio.ipv4_address==None):
+                try:
+                    print("Connecting to %s ..."%secrets["ssid"])
+                    wifi.radio.connect(secrets["ssid"], secrets["password"])
+                    print("Connected to %s!"%secrets["ssid"])
+                    pool = socketpool.SocketPool(wifi.radio)
+                    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+                except:
+                    print("can't connect to wifi")
+                
+            if (wifi.radio.ipv4_address):
+                for sensor in sensors:
+                    if len(sensor)==2:
+                        sensorID=sensor[0]
+                        node_id=0
+                        temp=float(sensor[1])*9./5.+32.
+                        print("RSSI:",rfm9x.last_rssi)
+                        print(sensorID,temp)
+                        if sensorID=='186':
+                            node_id=1
+                        #node_id=sensorID
+                        json_data = {"private_key":private_key, "node_id":node_id,"temperature_c":temp}
+
+                        # check wifi connect; connect if disconnected
+                        
+                        # post data to bayou
+                        print("Posting data...")
+                        response = None
+                        while not response:
+                            try:
+                                response = requests.post(JSON_POST_URL, json=json_data)
+                                failure_count = 0
+                                print(response.text)
+                            except AssertionError as error:
+                                print("Request failed, retrying...\n", error)
+                                failure_count += 1
+                                if failure_count >= attempts:
+                                    raise AssertionError(
+                                        "Failed to resolve hostname, \
+                                                        please check your router's DNS configuration."
+                                    ) from error
+                                continue
+            #else:
+            #    print("no wifi connection")
+
+                
+        except:
+            print("error")
