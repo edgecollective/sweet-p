6371c27d83b5a7e8ada8adb9423f81d6eda15658
Don Blair <donblair@gmail.com>
2023-08-16 22:49:01 -0400

    update

---
+++ ver_0.3/firmware/v1.0/base_tracker/secrets.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'Fios-7S5yS_EXT',
+    'password' : 'XXXXXX',
+    'bayou_public' : '35tmkbqumaz7',
+    'bayou_private' : 'XXXXXXX',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
