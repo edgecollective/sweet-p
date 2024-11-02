43ee193b6db6b01589caa7915ebd795a2a654e45
Don Blair <donblair@gmail.com>
2024-11-01 21:52:36 -0400

    updated ver 0.4 sweet-p

---
+++ ver_0.4/grassnomads_deployment_aug_2023/base/secrets_example.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'XXXXXX',
+    'password' : 'XXXXXX',
+    'bayou_public' : 'XXXXX',
+    'bayou_private' : 'XXXXXXX',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
