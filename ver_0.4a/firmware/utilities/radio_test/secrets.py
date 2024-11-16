753e27b36ea684db6e6dfb6383d4b153f14a06af
Don Blair <donblair@gmail.com>
2024-11-15 19:00:09 -0500

    added force send button

---
+++ ver_0.4a/firmware/utilities/radio_test/secrets.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'Fios-7S5yS_EXT',
+    'password' : 'whose44gel62has',
+    'bayou_public' : 'hynj3bxfuzst',
+    'bayou_private' : 'gyh3qfryma8q',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    }
